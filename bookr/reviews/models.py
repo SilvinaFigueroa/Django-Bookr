1c0e63f69b8147e01a946693d7cbd1ad4e58e7e2
SilvinaFigueroa <86253843+SilvinaFigueroa@users.noreply.github.com>
2023-05-05 20:48:04 -0700

    Upload CSV to database

--- bookr/reviews/models.py
+++ bookr/reviews/models.py
@@ -1,6 +1,8 @@
 # A Django model is essentially a Python class that holds the blueprint for creating a table in a database.
-
+# Each class is a table on the database
+from django.contrib import auth
 from django.db import models
+
 
 class Publisher(models.Model):
     """A company that publishes books."""
@@ -8,6 +10,10 @@ class Publisher(models.Model):
                             help_text="The name of the Publisher.")
     website = models.URLField(help_text="The Publisher's website.")
     email = models.EmailField(help_text="The Publisher's email address.")
+
+    def __str__(self):
+        return self.name
+
 
 class Book(models.Model):
     """A published book."""
@@ -17,6 +23,15 @@ class Book(models.Model):
         verbose_name="Date the book was published.")
     isbn = models.CharField(max_length=20,
                             verbose_name="ISBN number of the book.")
+    publisher = models.ForeignKey(Publisher,
+                                  on_delete=models.CASCADE)
+    contributors = models.ManyToManyField('Contributor',
+                                          through="BookContributor")
+
+    def __str__(self):
+        return self.title
+
+
 class Contributor(models.Model):
     """A contributor to a Book, e.g. author, editor, co-author."""
     first_names = models.CharField(max_length=50,
@@ -25,7 +40,10 @@ class Contributor(models.Model):
                                   help_text="The contributor's last name or names.")
     email = models.EmailField(help_text="The contact email for the contributor.")
 
+    def __str__(self):
+        return self.first_names
 class BookContributor(models.Model):
+    """ intermediary table  to establish relationships by using foreign keys to both the Book and Contributor tables """
     class ContributionRole(models.TextChoices):
         AUTHOR = "AUTHOR", "Author"
         CO_AUTHOR = "CO_AUTHOR", "Co-Author"
@@ -35,3 +53,15 @@ class BookContributor(models.Model):
     contributor = models.ForeignKey(Contributor, on_delete=models.CASCADE)
     role = models.CharField(verbose_name="The role this contributor had in the book.",
                             choices=ContributionRole.choices, max_length=20)
+
+
+class Review(models.Model):
+    content = models.TextField(help_text="The Review text.")
+    rating = models.IntegerField(help_text="The rating the reviewer has given.")
+    date_created = models.DateTimeField(auto_now_add=True,
+                                        help_text="The date and time the review was created.")
+    date_edited = models.DateTimeField(null=True,
+                                       help_text="The date and time the review was last edited.")
+    creator = models.ForeignKey(auth.get_user_model(), on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE,
+                             help_text="The Book that this review is for.")
