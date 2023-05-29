76fa428b7cca7c73a96564b0a5c99d00ac3981a0
SilvinaFigueroa <86253843+SilvinaFigueroa@users.noreply.github.com>
2023-05-29 13:19:18 -0700

    Seach form implementation http://127.0.0.1:8000/book-search/

--- bookr/reviews/urls.py
+++ bookr/reviews/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 
-
-urlpatterns = [path('books/', views.book_list, \
-                    name='book_list'),
-
-               path('book/<int:id>/', views.book_details, \
-                    name='book_details')]
+urlpatterns = [
+    path('', views.index),
+    path('books/', views.book_list, name='book_list'),
+    path('book/<int:id>/', views.book_details, name='book_details'),
+    path('book-search/', views.book_search, name='book_search'),
+]
--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -1,8 +1,45 @@
-from django.db.models import Avg
+from django.db.models import Avg, Q
 from django.db.models.functions import Round
 from django.shortcuts import render, get_object_or_404
+
+from .forms import SearchForm
 from .models import *
 from .utils import average_rating
+
+
+def index(request):
+    return render(request, "base.html")
+
+
+def book_search(request):
+    search_input = request.GET.get("search", "")
+    # SearchForm instance with GET request
+    form = SearchForm(request.GET)
+    # list to add the search results
+    search_results = []
+
+    # validate the form
+    if form.is_valid() and form.cleaned_data["search"]:
+        # get data from field set in form.py
+        search = form.cleaned_data["search"]
+        search_in = form.cleaned_data.get("search_in")
+
+        if search_in == "title":
+            search_results = Book.objects.filter(title__icontains=search)
+
+        else:
+            # Using a Q object to combine conditions and retrieve contributors from DB
+
+            if search_in == "title":
+                search_results = Book.objects.filter(title__icontains=search)
+            else:
+                search_results = Book.objects.filter(
+                    Q(contributors__first_names__icontains=search) |
+                    Q(contributors__last_names__icontains=search)
+                )
+
+    return render(request, "reviews/book-search.html",
+                  {'form': form, 'search_input': search_input, 'search_results': search_results})
 
 
 def book_list(request):
@@ -35,7 +72,6 @@ def book_details(request, id):
     review_list = []
 
     for element in reviews:
-
         content = element.content
         rating = element.rating
         date_created = element.date_created
